54269df262b0b0763cbd8635f598a765a89c2ef6
Sharon <schang@webadvanced.com>
2013-01-28 17:59:57 -0800

    Updated assembly version info, added publish to nuget in build

--- Build.cmd
+++ Build.cmd
@@ -1,7 +1,11 @@
 @echo off
-
+if "%1"==":publish" goto publish
 
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe buildscripts\build.proj %*
+goto end
 
-pause
+:publish
+powershell.exe -noprofile buildscripts\publish-nuget-packages.ps1
+
+goto end
 :end
